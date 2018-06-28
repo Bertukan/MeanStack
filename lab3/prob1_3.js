1f5a4831ad1e23b2b30c6584a69638bf85f6c79c
bertukan <bertukan2017@gmail.com>
2018-06-28 13:54:13 -0500

    prob3updated

---
+++ lab3/prob1_3.js
@@ -0,0 +1,16 @@
+
+const {promisify} = require('util')
+const dns = require('dns')
+const res = promisify(dns.resolve4);
+async function main() {
+
+    try{
+       const data= await res('www.mum.edu')
+        console.log(data);
+    }
+    catch(err){
+        console.log('error:', err);
+    }
+}
+
+main();
