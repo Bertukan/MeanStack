2b0b398771881c64770e7b6c2242e6d4e9bb98fd
bertukan <bertukan2017@gmail.com>
2018-06-28 13:34:52 -0500

    updating

---
+++ lab3/prob1_1.js
@@ -0,0 +1,7 @@
+const dns = require('dns')
+ dns.resolve4('www.mum.edu', function (err, addresses) {
+console.log(addresses)
+})
+
+
+
---
+++ lab3/prob1_2.js
@@ -0,0 +1,20 @@
+// const dns = require('dns')
+// const {promisify} = require('util')
+// const res = promisify(dns.resolve4);
+//  res('www.mum.edu').then((add)=>{console.log(add);}).catch(err=>console.log(err))
+
+
+
+
+
+// or another way we can do it like this
+
+// const util = require('util');
+// const dnsy = require('dns');
+
+// const stat = util.promisify(dnsy.resolve4);
+// stat('www.mum.edu').then((address) => {
+//     console.log('address: %j', address);
+// }).catch((error) => {
+//     console.log('Adress Not found');
+// });
