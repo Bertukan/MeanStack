8f03a63dc2099e0d108ecc68c0595743c6e4d1a8
bertukan <bertukan2017@gmail.com>
2018-06-28 13:11:28 -0500

    prob2

---
+++ lab3/prob2.js
@@ -0,0 +1,28 @@
+const server = require('http')
+const fs = require('fs')
+
+// server.createServer(function(req,res){
+//     res.writeHead(200,{'Content-Type':'image/jpg'});
+// var readStream = fs.createReadStream(__dirname+'/animal.jpg')
+//  readStream.pipe(res)
+// }).listen(8080);
+
+
+// server.createServer(function(req,res){
+//     fs.readFile('animal.jpg', function(err, data) {
+//         res.writeHead(200,{'Content-Type':'image/jpg'});
+//         res.write(data);
+//         res.end();
+//       });
+
+// }).listen(8080);
+
+server.createServer(function(req,res){
+    fs.readFile('animal.jpg', function(err, data) {
+        var b
+        res.writeHead(200,{'Content-Type':'image/jpg'});
+        res.write(data);
+        res.end();
+      });
+
+}).listen(8080);
